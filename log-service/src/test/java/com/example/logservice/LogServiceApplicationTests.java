42d73b9b013b5740512488fd157b71d255660fec
uok23 <unto.valkonen@outlook.com>
2020-05-03 15:37:04 +0300

    1 commit

--- log-service/src/test/java/com/example/logservice/LogServiceApplicationTests.java
+++ log-service/src/test/java/com/example/logservice/LogServiceApplicationTests.java
@@ -31,7 +31,7 @@ public class LogServiceApplicationTests {
     @Test
     public void loginTest() {
 
-        LoginRequestPayload loginRequestPayload = new LoginRequestPayload("060157-171W", "Meitsi")
+        LoginRequestPayload loginRequestPayload = new LoginRequestPayload("123456-789", "Meitsi");
 
         RestAssured
                 .given()
